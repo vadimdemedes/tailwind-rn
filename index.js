72627b2a376ddd8139191c0802bb1b620ddcb7e4
MateusAndrade <mateus.andrade47@outlook.com>
2020-03-09 16:36:53 -0300

    fix: changing return due to lint errors

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const styles = require('./styles.json');
 // "bg-green-100 text-green-800 font-semibold")
 // and receive a styles object for use in React Native views
 const tailwind = classNames => classNames.split(' ').reduce((currentStyles, className) => {
-	if (styles[className]) {
-		return {...currentStyles, ...styles[className]};
-	} else {
+	if (!styles[className]) {
 		console.warn(`Unsupported Tailwind class: "${className}"`);
-		return currentStyles;
 	}
+
+	return {...currentStyles, ...styles[className]};
 }, {});
 
 // Pass the name of a color (e.g. "blue-500") and receive a color value (e.g. "#4399e1")
