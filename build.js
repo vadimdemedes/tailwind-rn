8605be3caebcfcc30e240320b270c44b3c8a27d8
mattapperson <me@mattapperson.com>
2020-05-22 22:18:43 -0400

    add support for shadows

--- build.js
+++ build.js
@@ -1,8 +1,83 @@
 'use strict';
 const css = require('css');
 const cssToReactNative = require('css-to-react-native').default;
-
 const remToPx = value => `${Number.parseFloat(value) * 16}px`;
+
+function translateValues(value) {
+	let translatedValue = value;
+
+	if (translatedValue === 'transparent') {
+		return 'rgba(0,0,0,0)';
+	}
+
+	if (typeof translatedValue !== 'string') {
+		return translatedValue;
+	}
+
+	if (value.search(/^-?[0-9]+$/) !== -1) {
+		return parseInt(translatedValue);
+	}
+
+	if (value.search(/-?\.[0-9]+$/) !== -1) {
+		return parseFloat(translatedValue);
+	}
+
+	if (value.search(/^[0-9]+$/) !== -1) {
+		return parseInt(translatedValue);
+	}
+
+	return translatedValue;
+}
+
+function convertShadow(rule) {
+	if (rule.declarations.length > 1) {
+		throw new Error(
+			'tailwind-rn assums shadows are a box-shadow with a single shorthand value, something more complex was found'
+		);
+	}
+
+	let results, color, elevation;
+
+	if (
+		rule.declarations[0].value === 'none' ||
+		rule.declarations[0].value.search(/inset/) !== -1
+	) {
+		return {
+			shadowColor: 'rgba(0, 0, 0, 0)',
+			shadowOffset: {width: 0, height: 0},
+			shadowRadius: 0,
+			shadowOpacity: 0,
+			elevation: 0
+		};
+	}
+
+	results = rule.declarations[0].value.match(
+		/^([0-9]+)p?x?\s([0-9]+)p?x?\s([0-9]+)p?x?\s(-?[0-9]+)?p?x?\s?(rgba?\(.+?\))?(#[a-zA-Z0-9]{3,8})?/
+	);
+
+	elevation = rule.declarations[0].value.match(/,(?:\s+)?(-?[0-9]+)$/);
+
+	color = results[5];
+
+	elevation = elevation
+		? translateValues(elevation[1])
+		: translateValues(results[3]) / 2;
+
+	if (typeof color === 'undefined') {
+		color = results[6];
+	}
+
+	return {
+		shadowColor: color,
+		shadowOffset: {
+			width: translateValues(results[1]),
+			height: translateValues(results[2])
+		},
+		shadowRadius: translateValues(results[3]),
+		shadowOpacity: 1,
+		elevation: Math.round(elevation)
+	};
+}
 
 const getStyles = rule => {
 	const styles = rule.declarations
@@ -21,7 +96,6 @@ const getStyles = rule => {
 
 			return [property, value];
 		});
-
 	return cssToReactNative(styles);
 };
 
@@ -81,12 +155,18 @@ const supportedUtilities = [
 	// Opacity
 	/^opacity-/,
 	// Pointer events
-	/^pointer-events-/
+	/^pointer-events-/,
+	/^shadow-/
 ];
 
 const isUtilitySupported = utility => {
 	// Skip utilities with `currentColor` values
 	if (['border-current', 'text-current'].includes(utility)) {
+		return false;
+	}
+
+	// skip inner shadows
+	if (['shadow-outline', 'shadow-inner'].includes(utility)) {
 		return false;
 	}
 
@@ -115,7 +195,11 @@ module.exports = source => {
 				const utility = selector.replace(/^\./, '').replace('\\/', '/');
 
 				if (isUtilitySupported(utility)) {
-					styles[utility] = getStyles(rule);
+					if (/^shadow-/.test(utility)) {
+						styles[utility] = convertShadow(rule);
+					} else {
+						styles[utility] = getStyles(rule);
+					}
 				}
 			}
 		}
