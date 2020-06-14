9d5c28dc2966b7d84c5405b2d9cbddbc61b7581c
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2020-06-14 01:39:39 -0300

    Support custom colors on cli

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 const fs = require('fs');
+const path = require('path');
 const meow = require('meow');
 const postcss = require('postcss');
 const tailwind = require('tailwindcss');
@@ -17,10 +18,35 @@ const source = `
 @tailwind utilities;
 `;
 
+/**
+ * @returns {String[]} A list of provided custom colors
+ */
+const getCustomColors = () => {
+	try {
+		const config = require(path.resolve('tailwind.config'));
+
+		if (
+			config &&
+			config.theme &&
+			config.theme.extend &&
+			config.theme.extend.colors
+		) {
+			return Object.keys(config.theme.extend.colors);
+		}
+	} catch (error) {
+		if (error.code !== 'MODULE_NOT_FOUND') {
+			throw error;
+		}
+	}
+
+	return [];
+};
+
 postcss([tailwind])
 	.process(source, {from: undefined})
 	.then(({css}) => {
-		const styles = build(css);
+		const customColors = getCustomColors();
+		const styles = build(css, customColors);
 		fs.writeFileSync('styles.json', JSON.stringify(styles, null, '\t'));
 	})
 	.catch(error => {
