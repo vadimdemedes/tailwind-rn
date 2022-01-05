eaf0cf804aaffd0854d1c895a28251b4a50db620
Vadim Demedes <vadimdemedes@hey.com>
2022-01-06 00:05:28 +0200

    Batch compile CSS using `dataloader`

--- tests/helpers/tailwind-test.js
+++ tests/helpers/tailwind-test.js
@@ -1,10 +1,16 @@
 /* eslint-disable ava/no-ignored-test-files */
 const test = require('ava');
+const DataLoader = require('dataloader');
 const compile = require('./compile');
+
+const compiler = new DataLoader(async classNames => {
+	const {tailwind} = await compile(classNames.join(' '));
+	return classNames.map(_ => tailwind);
+});
 
 const tailwindTest = (classNames, expected) => {
 	test(classNames, async t => {
-		const {tailwind} = await compile(classNames);
+		const tailwind = await compiler.load(classNames);
 		t.deepEqual(tailwind(classNames), expected);
 	});
 };
