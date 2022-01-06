ea305e60dddcb734e55fddf638060bf140886596
Vadim Demedes <vadimdemedes@hey.com>
2022-01-06 15:11:24 +0200

    Fix lint errors

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-export default function tailwind(classNames: string): {[key: string]: string};
+export default function tailwind(classNames: string): Record<string, string>;
 export function getColor(color: string): string;
-export function create(styles: object): {
-	tailwind: (classNames: string) => {[key: string]: string};
+export function create(styles: Record<string, unknown>): {
+	tailwind: (classNames: string) => Record<string, string>;
 	getColor: (color: string) => string;
 };
