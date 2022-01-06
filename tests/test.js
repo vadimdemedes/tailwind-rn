010c597d1e0ab79233507e041071f375069d42ac
Vadim Demedes <vadimdemedes@hey.com>
2022-01-06 15:13:06 +0200

    Remove unnecessary tests

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,6 @@
 const test = require('ava');
 const compile = require('./helpers/compile');
 
-test('get styles for one class', async t => {
-	const classNames = 'text-blue-500';
-	const {tailwind} = await compile(classNames);
-	t.deepEqual(tailwind(classNames), {color: 'rgb(59 130 246 / 1)'});
-});
-
 test('get styles for multiple classes', async t => {
 	const classNames = 'text-blue-500 bg-blue-100';
 	const {tailwind} = await compile(classNames);
@@ -23,34 +17,6 @@ test('ignore unknown classes', async t => {
 
 	t.deepEqual(tailwind(classNames), {
 		color: 'rgb(59 130 246 / 1)'
-	});
-});
-
-test('support color opacity', async t => {
-	const classNames =
-		'text-blue-500 text-opacity-50 bg-blue-100 bg-opacity-50 border-blue-100 border-opacity-50';
-	const {tailwind} = await compile(classNames);
-
-	t.deepEqual(tailwind(classNames), {
-		color: 'rgb(59 130 246 / 0.5)',
-		backgroundColor: 'rgb(219 234 254 / 0.5)',
-		borderTopColor: 'rgb(219 234 254 / 0.5)',
-		borderRightColor: 'rgb(219 234 254 / 0.5)',
-		borderBottomColor: 'rgb(219 234 254 / 0.5)',
-		borderLeftColor: 'rgb(219 234 254 / 0.5)'
-	});
-});
-
-test('ignore non-string values when transforming CSS variables', async t => {
-	const classNames = 'bg-blue-500 p-12';
-	const {tailwind} = await compile(classNames);
-
-	t.deepEqual(tailwind(classNames), {
-		backgroundColor: 'rgb(59 130 246 / 1)',
-		paddingTop: 48,
-		paddingRight: 48,
-		paddingBottom: 48,
-		paddingLeft: 48
 	});
 });
 
