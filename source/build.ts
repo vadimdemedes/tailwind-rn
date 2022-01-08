b96edb70ebbdf07f4c9bb19d711ade7915f73922
Vadim Demedes <vadimdemedes@hey.com>
2022-01-08 21:00:26 +0200

    Delete detection of unsupported utilities

--- source/build.ts
+++ source/build.ts
@@ -4,164 +4,17 @@ import remToPx from './lib/rem-to-px';
 import {Styles} from './types';
 
 const getStyles = (rule: css.Rule) => {
-	const styles: StyleTuple[] = (
-		rule.declarations as Array<Required<css.Declaration>>
-	)
-		.filter(({property, value}) => {
-			// Skip line-height utilities without units
-			if (property === 'line-height') {
-				return typeof value === 'string' && value.endsWith('rem');
-			}
+	const declarations = rule.declarations as Array<Required<css.Declaration>>;
 
-			return true;
-		})
-		.map(({property, value}) => {
-			if (typeof value === 'string' && value.endsWith('rem')) {
-				return [property, remToPx(value)];
-			}
+	const styles: StyleTuple[] = declarations.map(({property, value}) => {
+		if (typeof value === 'string' && value.endsWith('rem')) {
+			return [property, remToPx(value)];
+		}
 
-			return [property, value];
-		});
+		return [property, value];
+	});
 
 	return cssToReactNative(styles);
-};
-
-const unsupportedProperties = new Set([
-	'box-sizing',
-	'float',
-	'clear',
-	'object-fit',
-	'object-position',
-	'overflow-x',
-	'overflow-y',
-	'-webkit-overflow-scrolling',
-	'overscroll-behavior',
-	'overscroll-behavior-x',
-	'overscroll-behavior-y',
-	'visibility',
-	'order',
-	'grid-template-columns',
-	'grid-column',
-	'grid-column-start',
-	'grid-column-end',
-	'grid-template-rows',
-	'grid-row',
-	'grid-row-start',
-	'grid-row-end',
-	'grid-auto-flow',
-	'grid-auto-columns',
-	'grid-auto-rows',
-	'gap',
-	'column-gap',
-	'row-gap',
-	'justify-items',
-	'justify-self',
-	'place-content',
-	'place-items',
-	'place-self',
-	'font-variant-numeric',
-	'list-style-type',
-	'list-style-position',
-	'vertical-align',
-	'white-space',
-	'word-break',
-	'background-attachment',
-	'background-clip',
-	'background-position',
-	'background-repeat',
-	'background-size',
-	'background-image',
-	'border-collapse',
-	'table-layout',
-	'box-shadow',
-	'transition-property',
-	'transition-duration',
-	'transition-timing-function',
-	'transition-delay',
-	'animation',
-	'transform',
-	'transform-origin',
-	'appearance',
-	'cursor',
-	'outline',
-	'resize',
-	'user-select',
-	'fill',
-	'stroke',
-	'stroke-width'
-]);
-
-const supportedPropertiesWithAuto = new Set([
-	'align-self',
-	'pointer-events',
-	'margin-top',
-	'margin-right',
-	'margin-bottom',
-	'margin-left'
-]);
-
-const isUtilitySupported = (utility: string, rule: css.Rule) => {
-	// Skip unsupported utilities
-	if (
-		[
-			'clearfix',
-			'antialiased',
-			'subpixel-antialiased',
-			'sr-only',
-			'not-sr-only'
-		].includes(utility) ||
-		/^(space|placeholder|from|via|to|divide)-/.test(utility) ||
-		/^-?(scale|rotate|translate|skew)-/.test(utility)
-	) {
-		return false;
-	}
-
-	if (!Array.isArray(rule.declarations)) {
-		return false;
-	}
-
-	// Skip utilities with unsupported properties
-	for (const declaration of rule.declarations) {
-		const {property, value} = declaration as Required<css.Declaration>;
-
-		if (unsupportedProperties.has(property)) {
-			return false;
-		}
-
-		if (property === 'display' && !['flex', 'none'].includes(value)) {
-			return false;
-		}
-
-		if (
-			property === 'overflow' &&
-			!['visible', 'hidden', 'scroll'].includes(value)
-		) {
-			return false;
-		}
-
-		if (property === 'position' && !['absolute', 'relative'].includes(value)) {
-			return false;
-		}
-
-		if (property === 'line-height' && !value.endsWith('rem')) {
-			return false;
-		}
-
-		if (
-			(value === 'auto' && !supportedPropertiesWithAuto.has(property)) ||
-			value.endsWith('vw') ||
-			value.endsWith('vh') ||
-			value === 'currentColor'
-		) {
-			return false;
-		}
-
-		if (property === 'text-decoration-line' && value === 'overline') {
-			return false;
-		}
-	}
-
-	return true;
 };
 
 const build = (source: string) => {
@@ -182,12 +35,10 @@ const build = (source: string) => {
 		for (const selector of rule.selectors) {
 			const utility = selector.replace(/^\./, '').replace(/\\/g, '');
 
-			if (isUtilitySupported(utility, rule)) {
-				styles[utility] = {
-					style: getStyles(rule),
-					media
-				};
-			}
+			styles[utility] = {
+				style: getStyles(rule),
+				media
+			};
 		}
 	};
 
